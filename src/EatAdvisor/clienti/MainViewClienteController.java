419e394c67b25af82658e823906829c5c30230af
retr00h <34344402+retr00h@users.noreply.github.com>
2021-01-28 13:42:18 +0100

    Sistemato un problema per il quale il pulsante di login risultava non funzionante.

--- src/EatAdvisor/clienti/MainViewClienteController.java
+++ src/EatAdvisor/clienti/MainViewClienteController.java
@@ -47,21 +47,25 @@ public class MainViewClienteController {
     }
 
     public void initialize() {
-        loginButton.setOnMouseClicked(event -> login(true));
+//        loginButton.setOnMouseClicked(event -> login(true));
         continueButton.setOnMouseClicked(event -> login(false));
 
         nicknameField.setOnKeyReleased(event -> {
             if (!nicknameField.getText().equals("")) {
+                // se il nicknameField non è vuoto, l'utente vuole loggarsi o registrarsi
                 if (EatAdvisor.validate(nicknameField.getText(), 10)) {
+                    // se il nickname è valido, controllo che l'utente sia registrato
                     continueButton.setVisible(false);
                     continueLabel.setVisible(false);
                     if (EatAdvisor.isRegistrato(nicknameField.getText(), 1)) {
+                        // se l'utente è registrato, attendo l'inserimento della password
                         registerText.setVisible(false);
                         confirmPasswordField.setVisible(false);
                         confirmPasswordLabel.setVisible(false);
-                        loginButton.setDisable(false);
+//                        loginButton.setDisable(false);
                         login(true);
                     } else {
+                        // questo ramo viene eseguito se l'utente NON è registrato
                         loginButton.setDisable(true);
                         registerText.setVisible(true);
                         confirmPasswordField.setVisible(true);
@@ -71,6 +75,7 @@ public class MainViewClienteController {
                     }
                 }
             } else {
+                // questo ramo esegue se nicknameField è vuoto
                 continueButton.setText("Continua senza loggarti");
                 registerText.setVisible(false);
                 confirmPasswordField.setVisible(false);
@@ -109,8 +114,8 @@ public class MainViewClienteController {
     private void login(boolean haveToLogin) {
         if (haveToLogin) {
             if (checkNicknameAndPassword(nicknameField.getText(), passwordField.getText())) {
-                continueButton.setDisable(false);
-                continueButton.setOnMouseClicked(event1 -> {
+                loginButton.setDisable(false);
+                loginButton.setOnMouseClicked(event1 -> {
                     try {
                         Stage stage = (Stage) anchorPane.getScene().getWindow();
                         FXMLLoader loader = new FXMLLoader(getClass().getResource("LoggedUserView.fxml"));
@@ -131,6 +136,7 @@ public class MainViewClienteController {
             } else {
                 continueButton.setDisable(true);
                 continueLabel.setVisible(false);
+
             }
         } else {
             try {
